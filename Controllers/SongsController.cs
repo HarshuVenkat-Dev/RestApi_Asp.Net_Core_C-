ecdb2935a3311db2ba15cb65b767a08680ce0106
Harshini Venkatesan <Harshini.Venkatesan@ATMECS.CORP>
2025-02-17 19:29:03 +0530

    Initial commit on RestApi-Asp.NetCore Project

---
+++ Controllers/SongsController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicApi.Data;
+using MusicApi.Models;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MusicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private ApiDbContext _dbContext;
+        public SongsController(ApiDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<SongsController>
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return _dbContext.Songs;
+        }
+
+        // GET api/<SongsController>/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var song = _dbContext.Songs.Find(id);
+            if(song == null)
+            {
+                return NotFound("Record not found");
+            }
+            else
+            {
+                return Ok(song);
+            }
+        }
+            
+        // POST api/<SongsController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Song song)
+        {
+            _dbContext.Songs.Add(song);
+            _dbContext.SaveChanges();
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        // PUT api/<SongsController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Song songObj)
+        {
+            var song = _dbContext.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound("No record found in this id");
+            }
+            else
+            {
+                song.Title = songObj.Title;
+                song.Language = songObj.Language;
+                _dbContext.SaveChanges();
+                return Ok("Record updated successfully");
+            }
+        }
+
+        // DELETE api/<SongsController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var song = _dbContext.Songs.Find(id);
+            if (song == null)
+            {
+                return NotFound("Record not found");
+            }
+            else
+            {
+                _dbContext.Songs.Remove(song);
+                _dbContext.SaveChanges();
+                return Ok("Record deleted");
+            }     
+        }
+    }
+}
